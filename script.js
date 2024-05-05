86e637928572af165bb35be6ad3142e3c8c7012b
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-05 15:44:57 +0530

    ADD

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	if(str.trim()==0) return 0;
+	const encoder = new TextEncoder();
+const encodedData = encoder.encode(str);
+const byteLength = encodedData.length;
+	return byteLength;
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+ const str = prompt("Enter some string.");
 alert(byteSize(str));
